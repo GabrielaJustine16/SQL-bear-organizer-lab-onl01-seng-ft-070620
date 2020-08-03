fc9da3c781aa90a8803cd111b3fac0ff76d01da3
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-03 23:35:42 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 16, "Male", "orange", "Aggressive", 2);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 16, "Female", "light brown", "Docile", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 17, "Female", "Black", "Playful", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "Male", "Brown", "Aggressive", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "Female", "yellow", "Playful", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 16, "Male", "Light Brown", "Aggressive", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy",13, "Male", "red", "Docile", 10);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "Male", "Brown", "Aggressive", 0);
